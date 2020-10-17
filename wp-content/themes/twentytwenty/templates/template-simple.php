17d4ca9a475b681c4a86ecf99a7f269e4e319e50
jepretarim <tarim.jepre@gmail.com>
2020-10-18 02:08:09 +0800

    Add custom page template

---
+++ wp-content/themes/twentytwenty/templates/template-simple.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Template Name: Simple Template
+* Template Post Type: post, page
+*
+ * @package WordPress
+* @subpackage Twenty_Twenty
+* @since Twenty Twenty 1.0
+*/
+
+get_header();
+?>
